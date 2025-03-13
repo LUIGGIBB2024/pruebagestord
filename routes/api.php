debf74c0803e5f5b47f62c01c2b5e1320f1fe27a
Luis Alberto Bernal B. <enlacevisual@hotmail.com>
2025-03-13 07:37:36 -0500

    Updated Software

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\RoutePath;
 
+Route::get('/test', function () {
+    return response()->json(['message' => 'Hello World!']);
+});
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
